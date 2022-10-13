5ee791a12275d5e74818b408fe1987e237ff3afc
jdun0012 <64505454+jdun0012@users.noreply.github.com>
2022-10-14 10:14:32 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,21 @@
+var vg_1 = "SpeciesAustralia.json";
+
+var vg_2 = "conservationStatus.json";
+
+var vg_3 = "FrequencyVsM.json";
+
+vegaEmbed("#line_chart", vg_1).then(function(result) {
+
+    
+ }).catch(console.error);
+
+ vegaEmbed("#bar_chart", vg_2).then(function(result) {
+
+    
+}).catch(console.error);
+
+vegaEmbed("#bar_chart2", vg_3).then(function(result) {
+
+    
+}).catch(console.error);
+    
