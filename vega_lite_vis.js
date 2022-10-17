ad23809a1e640fac66d4234a6f33cd5f2fb43dd5
jdun0012 <64505454+jdun0012@users.noreply.github.com>
2022-10-17 16:05:35 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,21 +1,36 @@
-var vg_1 = "SpeciesAustralia.json";
+var vg_1 = "BirdHabitat.json";
 
-var vg_2 = "conservationStatus.json";
+var vg_2 = "BirdHabitatGlobal.json";
 
-var vg_3 = "FrequencyVsM.json";
+var vg_3 = "Frequency.json";
+
+var vg_4 = "Price.json";
+
+var vg_5 = "occurances.json";
 
 vegaEmbed("#line_chart", vg_1).then(function(result) {
 
     
  }).catch(console.error);
 
- vegaEmbed("#bar_chart", vg_2).then(function(result) {
+ vegaEmbed("#chart2", vg_2).then(function(result) {
 
     
 }).catch(console.error);
 
-vegaEmbed("#bar_chart2", vg_3).then(function(result) {
+vegaEmbed("#chart3", vg_3).then(function(result) {
 
     
 }).catch(console.error);
+
+vegaEmbed("#chart4", vg_4).then(function(result) {
+
     
+}).catch(console.error);
+
+vegaEmbed("#chart5", vg_5).then(function(result) {
+
+    
+}).catch(console.error);
+
+
